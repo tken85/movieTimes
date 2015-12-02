0da5b1a7c993d35accbd2b45cea1cabd25e70471
tken85 <terrykennair@Terrys-MacBook-Pro.local>
2015-12-02 12:39:38 -0500

    changed to app folder and server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var port = process.env.PORT || 3001;
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(Server);
+
+
+app.use(express.static(__dirname + "/app"));
+
+app.get('/', function(request, response){
+  //response.send('<h1> i am king of node</h1>');
+  response.sendFile('app');
+});
+
+app.get('/maybe', function(req, res){
+  res.send("<p>What am i doint</p>");
+});
+
+http.listen(port); //call this after routes given
